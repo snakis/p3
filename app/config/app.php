42596d6bc4722c0a016ac95fb003a51e4298cb6b
Samantha Nakis <samantha.nakis@gmail.com>
2014-10-21 23:55:20 -0400

    added packages for lorem ipsum and random user and set up README.me

--- app/config/app.php
+++ app/config/app.php
@@ -188,7 +188,8 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
-
+		'faker'			  => 'fzaninotto\faker',
+		'lorem-ipsum'	  => 'badcow\lorem-ipsum',
 	),
 
 );
